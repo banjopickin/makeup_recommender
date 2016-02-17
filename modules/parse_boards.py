a45f7219f12938bbddf93225030f3250c881f334
banjopickin <yihualeng@gmail.com>
2016-02-16 18:05:25 -0800

    in progress

--- modules/parse_boards.py
+++ modules/parse_boards.py
@@ -3,3 +3,15 @@ This file includes a set of functions which can parse the boards info parsed fro
 be saved under data directory.
 '''
 
+import re
+
+def parse_boards(lis):
+    '''
+    split the urls and parse the latter substring
+    :param lis: list
+    :return: lis
+    '''
+    outlis = []
+    for url in lis:
+        outlis.append(re.split("com",url))
+    return outlis
